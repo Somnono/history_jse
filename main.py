b95f710de445541f4b55b637ba0cad7e3bd6e610
Somnono <somnono@ymail.com>
2026-03-04 10:27:58 +0200

    structure

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import yfinance as yf
+import pandas as pd
 
-ticker_symbol = "NPN.JO"  # Naspers on JSE
+ticker_symbol = "NPN.JO"
 
 ticker = yf.Ticker(ticker_symbol)
 
-data = ticker.history(period="1y")
+data = ticker.history(period="5y")
+
+# Keep only Adjusted Close
+data = data[["Close"]]
+
+# Reset index so Date becomes a column
+data.reset_index(inplace=True)
 
 print(data.head())
